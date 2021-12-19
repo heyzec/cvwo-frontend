5cb6546e91458c4ff2842ff8434d9fb028315a0a
heyzec <61238538+heyzec@users.noreply.github.com>
2021-12-19 12:58:48 +0800

    Rough design of header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Task from './components/Task'
-import NextTask from './components/NextTask'
 import dayjs from 'dayjs'
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,42 @@
-const Header = () => (
-  <h1>
-    Your To Dos
-  </h1>
-)
+import {useState} from 'react'
+import { GoSearch } from 'react-icons/go'
+import { AiOutlineMenu } from 'react-icons/ai'
 
-export default Header
+const Header = () => {
+  
+  const [active, setActive] = useState(false)
+
+  const toggleMenu = () => {
+    console.log("CALLED")
+    setActive(!active)
+  }
+
+  return (
+    <header id="header">
+      <div id="header-title">
+        <h1>
+          Your To Dos
+        </h1>
+      </div>
+      <AiOutlineMenu id="header-toggle" size={25} onClick={toggleMenu} className="clickable" />
+      <div id="header-spacer"></div>
+      <div id="header-search">
+        <GoSearch />
+      </div>
+      <div id="header-date" className="header-elem">
+        <span>Sem 1 Vacation 1</span>
+      </div>
+      <div id="header-nav" className={active ? "active" : null}>
+        <div id="header-example" className="header-elem">
+          <span>Example</span>
+        </div>
+        <div id="header-login" className="header-elem">
+          <span>Sign up!</span>
+        </div>
+      </div>
+
+    </header>
+  )
+}
+
+  export default Header
